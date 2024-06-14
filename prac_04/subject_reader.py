cb9336328301bead47130482ca2133b74d75f4c2
jc959024 <huayu.zhong@my.jcu.edu.au>
2024-06-14 15:39:26 +0800

    commit

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,32 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
+    data = get_data()
     print(data)
+    display_new_data(data)
 
 
-def load_data():
+def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+    teachers_information = []
+    with open(FILENAME, 'r') as input_file:
+        for line in input_file:
+            # print(line)  # See what a line looks like
+            # print(repr(line))  # See what a line really looks like
+            line = line.strip()  # Remove the \n
+            parts = line.split(',')  # Separate the data into its parts
+            # print(parts)  # See what the parts look like (notice the integer is a string)
+            parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+            teachers_information.append(parts)
+            # print(parts)  # See if that worked
+            # print("----------")
+    return teachers_information
+
+
+def display_new_data(data):
+    """Display the details of each subject."""
+    for parts in data:
+        print(f"{parts[0]} is taught by {parts[1]} and has {parts[2]} students")
 
 
 main()
