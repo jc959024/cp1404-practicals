a31c88e9a370faff3ec61bab3e9ee5dd2b5fd02f
jc959024 <huayu.zhong@my.jcu.edu.au>
2024-07-14 18:49:07 +0800

    Change to constants

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -6,22 +6,19 @@ Actual:   14 minutes
 
 
 class Guitar:
+    CURRENT_YEAR = 2024
+    VINTAGE_AGE = 50
 
     def __init__(self, name="", year=0, cost=0):
-
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
-
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
     def get_age(self):
-
-        current_year = 2024
-        return current_year - self.year
+        return self.CURRENT_YEAR - self.year
 
     def is_vintage(self):
-
-        return self.get_age() >= 50
+        return self.get_age() >= self.VINTAGE_AGE
