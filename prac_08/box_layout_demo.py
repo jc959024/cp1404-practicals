bb89a2a2a7f9c4138658c3f72ebe2d3552d8cfff
jc959024 <huayu.zhong@my.jcu.edu.au>
2024-07-28 16:32:59 +0800

    “Hello”

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -2,15 +2,16 @@ from kivy.app import App
 from kivy.lang import Builder
 
 
-def handle_greet():
-    print('greet')
-
-
 class BoxLayoutDemo(App):
     def build(self):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print('test')
+        self.root.ids.output_label.text = "Hello"
+
 
 BoxLayoutDemo().run()
+
