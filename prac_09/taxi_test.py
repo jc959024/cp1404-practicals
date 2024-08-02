9966e374fdda55d3e20cca86ccfdc010893db1cb
jc959024 <huayu.zhong@my.jcu.edu.au>
2024-08-02 19:43:47 +0800

    change

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,19 +1,20 @@
 from taxi import Taxi
 
-# Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel
+# Create a new taxi object
 my_taxi = Taxi("Prius 1", 100)
 
-# Drive the taxi 40 km
+# Drive 40 km
 my_taxi.drive(40)
 
-# Print the taxi's details and the current fare
+# Print details and fare
 print(my_taxi)
 print(f"Current fare: ${my_taxi.get_fare():.2f}")
 
-# Restart the meter (start a new fare) and then drive the car 100 km
+# Start a new fare and drive 100 km
 my_taxi.start_fare()
 my_taxi.drive(100)
 
-# Print the details and the current fare
+# Print details and fare
 print(my_taxi)
 print(f"Current fare: ${my_taxi.get_fare():.2f}")
+
