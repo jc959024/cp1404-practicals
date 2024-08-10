cb74fab136bb0ba3af8a113ab0ea0b04e8f8841b
jc959024 <huayu.zhong@my.jcu.edu.au>
2024-08-10 21:32:17 +0800

    correct indentation problem in the test_car_fuel() function

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -3,7 +3,6 @@ CP1404/CP5632 Practical
 Testing demo using assert and doctest
 """
 
-
 import doctest
 from prac_06.car import Car
 
@@ -30,61 +29,56 @@ def run_tests():
     """Run the tests on the functions."""
     # assert test with no message - used to see if the function works properly
     assert repeat_string("Python", 1) == "Python"
-    # the test below should fail
+    # The test below should pass now that the function is fixed
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
-    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
-
-    # assert test with custom message,
+    # Assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
     # this should pass (no output)
     test_car = Car()
     assert test_car._odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
-    # Note that Car's __init__ function sets the fuel in one of two ways:
-    # using the value passed in or the default
-    # You should test both of these
-    test_car = Car(fuel=10)
-
- def test_car_fuel():
-        car_with_fuel = Car('WithFuel', 10)
+    # Test Car's fuel property with a test function
+    def test_car_fuel():
+        car_with_fuel = Car(fuel=10)
         assert car_with_fuel.fuel == 10, "Initial fuel not set correctly."
-        car_without_fuel = Car('WithoutFuel')
+        car_without_fuel = Car()
         assert car_without_fuel.fuel == 0, "Default fuel should be 0."
-    car_with_fuel = Car('WithFuel', 10)
-    assert car_with_fuel.fuel == 10, "Initial fuel not set correctly."
-    print("All test cases passed for Car fuel property")
-    car_without_fuel = Car('WithoutFuel')
-    assert car_without_fuel.fuel == 0, "Default fuel should be 0."
+
     test_car_fuel()
+    print("All tests passed.")
+
 
 run_tests()
 
-
-# TODO: 3. Uncomment the following line and run the doctests
-# (PyCharm may see your >>> doctest comments and run doctests anyway.)
+# Uncomment the following line to run doctests
 doctest.testmod()
 
-# TODO: 4. Fix the failing is_long_word function
-# (don't change the tests, change the function!)
 
-# TODO: 5. Write and test a function to format a phrase as a sentence,
-# starting with a capital and ending with a single full stop.
-# Important: start with a function header and just use pass as the body
-# then add doctests for 3 tests:
-# 'hello' -> 'Hello.'
-# 'It is an ex parrot.' -> 'It is an ex parrot.'
-# and one more you decide (one that is valid!)
-# test this and watch the tests fail
-# then write the body of the function so that the tests pass
+# Fixing the is_long_word function
+def is_long_word(word, length=5):
+    """
+    Determine if the word is as long or longer than the length passed in
+    >>> is_long_word("not")
+    False
+    >>> is_long_word("supercalifrag")
+    True
+    >>> is_long_word("Python", 6)
+    True
+    """
+    return len(word) >= length
+
+
+# Write and test a function to format a phrase as a sentence
 def format_phrase(words):
     if words[0].islower():
         words = words[0].upper() + words[1:]
     if words[-1] != ".":
         words += "."
     return words
+
+
+# Testing the format_phrase function
 assert format_phrase('hello') == 'Hello.'
 assert format_phrase('It is an ex parrot.') == 'It is an ex parrot.'
 assert format_phrase('Cars work.') == 'Cars work.'
